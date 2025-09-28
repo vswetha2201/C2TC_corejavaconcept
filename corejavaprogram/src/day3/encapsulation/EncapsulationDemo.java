f1465a3f84b5a2c4458693322e7ffcbf27a1adac
swetha v <swetha v vswetha22012004@gmail.com>
2025-09-28 17:36:16 +0530

    commit

---
+++ corejavaprogram/src/day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package day3.encapsulation;
+
+public class EncapsulationDemo {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+
+}
